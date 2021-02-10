4669deb6919781ebfa45ac9dbc909baee315872c
DevLikeMike <michael@MacBook-Pro.local>
2021-02-10 12:04:48 -0500

    Pre-production commit

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import colors from "colors";
 import dotenv from "dotenv";
-import helmet from "helmet";
+import path from "path";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
 // Route imports
@@ -14,7 +14,6 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(helmet());
 app.use(express.json());
 
 if (process.env.NODE_ENV === "development") {
@@ -25,12 +24,23 @@ if (process.env.NODE_ENV === "development") {
 app.use("/api/users", userRoutes);
 app.use("/api/games", gameRoutes);
 
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+}
+
 app.use(notFound);
 app.use(errorHandler);
 
 // Port declaration
 const PORT = process.env.PORT || 5000;
+const MODE = process.env.NODE_ENV;
 
-app.listen(5000, () => {
-  console.log(`Server started on port ${PORT}`.yellow.bold);
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT} in ${MODE} mode`.yellow.bold);
 });
